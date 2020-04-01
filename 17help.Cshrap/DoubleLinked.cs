2769ce1aa9077877b8f1929f625d33350462f68a
阿泰 <@LAPTOP-GIAMUU3O>
2020-04-01 15:43:44 +0800

    双向链表基本结构，有方法没实现 测试两个方法（插入）

--- 17help.Cshrap/DoubleLinked.cs
+++ 17help.Cshrap/DoubleLinked.cs
@@ -6,25 +6,48 @@ namespace ConsoleApp3
 {
     public class DoubleLinked
     {
-        //链表节点
+        ///链表需求：
+        ///怎么获取链表的长度
+        ///怎样指定当前节点
+        ///之前节点，之后节点
+        ///查找节点的方法
+        ///插入之前节点方法，之后节点方法
+        ///删除节点方法
+        ///判断链表是不是空的方法
+        ///判断是否越界的方法
+        ///交换两个节点位置的方法
+        ///确定头和尾的属性
+        ///黑窗口显示链表的方法
+        ///链表的排序（时间顺序，大小顺序）
+        ///判断插入的节点是不是尾和头
+
+
+        /// <summary>
+        /// 之前节点
+        /// </summary>
         public DoubleLinked Preivous { get; private set; }
+
+        /// <summary>
+        /// 之后节点
+        /// </summary>
         public DoubleLinked Next { get; private set; }
-        public int Value { get; set; }
 
+        public DoubleLinked Value { get; set; }
 
+        /// <summary>
+        /// 头节点
+        /// </summary>
         public bool IsHead
         {
-            get
-            {
-                return Preivous == null;
-            }
+            get => Preivous == null;
         }
+
+        /// <summary>
+        /// 尾节点
+        /// </summary>
         public bool IsTail
         {
-            get
-            {
-                return Next == null;
-            }
+            get => Next == null;
         }
 
         /// <summary>
@@ -35,6 +58,7 @@ namespace ConsoleApp3
         public DoubleLinked FindBy(int value)
         {
             //向上找一次
+
             //向下找一次
             return null;
         }
@@ -46,22 +70,32 @@ namespace ConsoleApp3
         public void InsretAfter(DoubleLinked node)
         {
             this.Preivous = node;
-            if (node.Next==null)
+            if (node.Next == null)
             {
                 node.Next = this;
             }
             else
             {
                 this.Next = node.Next;
-                this.Preivous = node;
-                node.Next = this;
                 this.Next.Preivous = this;
             }
         }
-
+        /// <summary>
+        /// 在node之前插入当前节点
+        /// </summary>
+        /// <param name="node"></param>
         public void InsertBefore(DoubleLinked node)
         {
-
+            this.Next = node;
+            if (node.Preivous == null)
+            {
+                node.Preivous = this;
+            }
+            else
+            {
+                this.Preivous = node.Preivous;
+                this.Preivous.Next = this;
+            }
         }
 
         /// <summary>
@@ -77,11 +111,35 @@ namespace ConsoleApp3
         /// </summary>
         /// <param name="a"></param>
         /// <param name="b"></param>
-        public void Swap(DoubleLinked a,DoubleLinked b)
+        public void Swap(DoubleLinked a, DoubleLinked b)
         {
 
         }
 
+        /// <summary>
+        /// 通过计算获得长度
+        /// </summary>
+        /// <returns></returns>
+        public int GetLingth(int Lingth)
+        {
+            return Lingth;
+        }
+
+        /// <summary>
+        /// 判断链表是否为空
+        /// </summary>
+        /// <returns></returns>
+        public bool IsNull()
+        {
+            if (Value != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
 
 
     }
---
+++ 17helpTests1/DoubleLinkedTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp3;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3.Tests
+{
+    [TestClass()]
+    public class DoubleLinkedTests
+    {
+        [TestMethod()]
+        public void FindByTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void InsretAfterTest()
+        {
+            DoubleLinked node = new DoubleLinked();
+            DoubleLinked current = new DoubleLinked();
+            Assert.AreEqual(node.Next, current.Preivous);
+            Assert.AreEqual(node.Preivous, current.Next);
+            Assert.AreEqual(current.Next, node.Preivous);
+            Assert.AreEqual(current.Preivous, node.Next);
+            Assert.AreEqual(current.IsHead, node.IsTail);
+            DoubleLinked _current = new DoubleLinked();
+            DoubleLinked current1 = new DoubleLinked();
+            Assert.AreEqual(_current.Next, current1.Preivous);
+            Assert.AreEqual(_current.IsHead, current1.IsTail);
+            Assert.AreEqual(current1.IsHead, _current.IsTail);
+            Assert.AreEqual(_current.IsHead, current.IsTail);
+            Assert.AreEqual(current.Next, _current.Preivous);
+        }
+
+        [TestMethod()]
+        public void InsertBeforeTest()
+        {
+            DoubleLinked node = new DoubleLinked();
+            DoubleLinked current = new DoubleLinked();
+            Assert.AreEqual(node.Next, current.Preivous);
+            Assert.AreEqual(node.Preivous, current.Next);
+            Assert.AreEqual(current.Next, node.Preivous);
+            Assert.AreEqual(current.Preivous, node.Next);
+            Assert.AreEqual(current.IsHead, node.IsTail);
+            DoubleLinked _current = new DoubleLinked();
+            Assert.AreEqual(_current.IsHead, current.IsTail);
+            Assert.AreEqual(current.Next, _current.Preivous);
+            Assert.AreEqual(_current.Preivous, current.Next);
+            DoubleLinked current1 = new DoubleLinked();
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SwapTest()
+        {
+            //Assert.Fail();
+        }
+    }
+}
