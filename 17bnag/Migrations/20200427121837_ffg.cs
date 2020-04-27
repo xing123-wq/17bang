1ffda4a16fb73bba6e7792c9920386f4e9f92b13
阿泰 <@LAPTOP-GIAMUU3O>
2020-04-27 21:25:03 +0800

    注册邀请人验证

---
+++ 17bnag/Migrations/20200427121837_ffg.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace _17bnag.Migrations
+{
+    public partial class ffg : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ValidatePassword",
+                table: "Users",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "VerificationCode",
+                table: "Users",
+                maxLength: 4,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "inviter",
+                table: "Users",
+                maxLength: 8,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ValidatePassword",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "VerificationCode",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "inviter",
+                table: "Users");
+        }
+    }
+}
