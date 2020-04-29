969eb2b43e5c96a1558212f77384d8225b52318c
阿泰 <@LAPTOP-GIAMUU3O>
2020-04-29 21:07:59 +0800

    让文章在Edits呈现内容

--- 17bnag/Pages/Article/Edits.cshtml.cs
+++ 17bnag/Pages/Article/Edits.cshtml.cs
@@ -2,16 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using _17bnag.Data;
+using _17bnag.Entitys;
+using _17bnag.Layout;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace _17bnag.Article
 {
-    public class EditsModel : PageModel
+    public class EditsModel : _LayoutModel
     {
+        public EditsModel(_17bnagContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public PublishArticle Article { get; set; }
         public void OnGet()
         {
-
+            int EditId = Convert.ToInt32(Request.RouteValues["id"]);
+            Article = GetPublishArticle(EditId);
+            base.SetLogOnStatus();
+        }
+        public void OnPost()
+        {
+        }
+        public PublishArticle GetPublishArticle(int Id)
+        {
+            return _context.PublishArticles.Where(a => a.Id == Id).SingleOrDefault();
         }
     }
 }
