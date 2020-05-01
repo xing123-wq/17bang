0df0565e8909b0dc0d7ebd755a2fc31e400eec9d
阿泰 <@LAPTOP-GIAMUU3O>
2020-05-01 20:54:10 +0800

    生成我的消息页面

---
+++ 17bnag/Pages/Message/Mine.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _17bnag.Data;
+using _17bnag.Layout;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _17bnag.Pages.Message
+{
+    public class MineModel : _LayoutModel
+    {
+        public MineModel(_17bnagContext context) : base(context)
+        {
+        }
+
+        public void OnGet()
+        {
+            ViewData["title"] = "我的消息-一起帮";
+            base.SetLogOnStatus();
+        }
+    }
+}
