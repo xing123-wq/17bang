c121fec8e1d95546d678f6ef088bfb12a029c7ec
龙泰 幸 <1978174217@qq.com>
2020-06-09 16:42:47 +0800

    解决对象为实例的异常

---
+++ 17bnag/Model/Log/OnModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _17bnag.Model.Log
+{
+    [BindProperties]
+    public class OnModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "邀请人(* 必填)")]
+        [Required(ErrorMessage = "* 邀请人不能为空")]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "* 用户名长度不能小于{2}也不能大于{1}")]
+        public string inviter { get; set; }
+        [Display(Name = "邀请码:（* 必填）")]
+        [Required(ErrorMessage = "* 邀请码不能为空")]
+        [MaxLength(4, ErrorMessage = "* 邀请码长度最大4位")]
+        [RegularExpression("[0-9]*")]
+        public string Invitationcode { get; internal set; }
+        public DateTime Time { get; set; }
+        public int HelpMony { get; set; }
+
+    }
+}
