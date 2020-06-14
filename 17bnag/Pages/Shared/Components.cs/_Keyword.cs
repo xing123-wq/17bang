0b39138207134422384ad5c8e9d0ac5ee1b5793b
龙泰 幸 <1978174217@qq.com>
2020-06-14 22:04:57 +0800

    关键字部分页

---
+++ 17bnag/Pages/Shared/Components.cs/_Keyword.cs
@@ -0,0 +1,24 @@
+﻿using _17bnag.Data;
+using _17bnag.Entitys;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _17bnag.Pages.Shared.Components.cs
+{
+    public class _Keyword : ViewComponent
+    {
+        private _17bnagContext _context { get; set; }
+        public _Keyword(_17bnagContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            IList<Keyword> keywords = _context.Keywords.ToList();
+            return View("_Keyword", keywords);
+        }
+    }
+}
