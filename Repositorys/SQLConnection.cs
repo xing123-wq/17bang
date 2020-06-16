1bd8d8d028887662e177d48c0779190846ffd60f
龙泰 幸 <1978174217@qq.com>
2020-06-16 21:05:51 +0800

    添加EF安装包

--- Repositorys/SQLConnection.cs
+++ Repositorys/SQLConnection.cs
@@ -1,14 +1,27 @@
-﻿using System;
+﻿using BLL;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Repositorys
 {
-    public class SQLConnection: DbContext
+    public class SQLConnection : DbContext
     {
-        
+        DbSet<User> Users { get; set; }
+        public SQLConnection() : base("17bang")
+        {
+#if DEBUG
+            Database.Log = S => File.AppendAllText("E:\\EF.log", S);
+#endif
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
