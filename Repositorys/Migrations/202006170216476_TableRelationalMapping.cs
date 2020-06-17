01e1ca388d7de4b3ccc808f8937a8759f90105ea
龙泰 幸 <1978174217@qq.com>
2020-06-17 10:21:07 +0800

    表的关系映射

---
+++ Repositorys/Migrations/202006170216476_TableRelationalMapping.cs
@@ -0,0 +1,28 @@
+﻿namespace Repositorys.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class TableRelationalMapping : DbMigration
+    {
+        public override void Up()
+        {
+            DropPrimaryKey("dbo.ArticleAndKeywords");
+            DropPrimaryKey("dbo.ProblemAndKeywords");
+            AlterColumn("dbo.ArticleAndKeywords", "Id", c => c.Int(nullable: false));
+            AlterColumn("dbo.ProblemAndKeywords", "Id", c => c.Int(nullable: false));
+            AddPrimaryKey("dbo.ArticleAndKeywords", new[] { "ArticleId", "KeywordId" });
+            AddPrimaryKey("dbo.ProblemAndKeywords", new[] { "ProblemId", "KeywordId" });
+        }
+        
+        public override void Down()
+        {
+            DropPrimaryKey("dbo.ProblemAndKeywords");
+            DropPrimaryKey("dbo.ArticleAndKeywords");
+            AlterColumn("dbo.ProblemAndKeywords", "Id", c => c.Int(nullable: false, identity: true));
+            AlterColumn("dbo.ArticleAndKeywords", "Id", c => c.Int(nullable: false, identity: true));
+            AddPrimaryKey("dbo.ProblemAndKeywords", "Id");
+            AddPrimaryKey("dbo.ArticleAndKeywords", "Id");
+        }
+    }
+}
--- Repositorys/SQLConnection.cs
+++ Repositorys/SQLConnection.cs
@@ -26,42 +26,42 @@ namespace Repositorys
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
 
-            //modelBuilder.Entity<ArticleAndKeyword>()
-            //    .HasKey(ak => new { aId = ak.ArticleId, kId = ak.KeywordId });
+            modelBuilder.Entity<ArticleAndKeyword>()
+                .HasKey(ak => new { ak.ArticleId, ak.KeywordId });
 
-            //modelBuilder.Entity<ArticleAndKeyword>()
-            //    .HasRequired(a => a.Article)
-            //    .WithMany(a => a.Keywords)
-            //    .HasForeignKey(a => a.ArticleId);
+            modelBuilder.Entity<ArticleAndKeyword>()
+                .HasRequired(a => a.Article)
+                .WithMany(a => a.Keywords)
+                .HasForeignKey(a => a.ArticleId);
 
 
-            //modelBuilder.Entity<ArticleAndKeyword>()
-            //   .HasRequired(ak => ak.Keyword)
-            //   .WithMany(ak => ak.Articles)
-            //   .HasForeignKey(ak => ak.KeywordId);
+            modelBuilder.Entity<ArticleAndKeyword>()
+               .HasRequired(ak => ak.Keyword)
+               .WithMany(ak => ak.Articles)
+               .HasForeignKey(ak => ak.KeywordId);
 
-            //modelBuilder.Entity<ProblemAndKeyword>()
-            //  .HasKey(pk => new { pId = pk.ProblemId, kId = pk.KeywordId });
+            modelBuilder.Entity<ProblemAndKeyword>()
+              .HasKey(pk => new { pk.ProblemId, pk.KeywordId });
 
-            //modelBuilder.Entity<ProblemAndKeyword>()
-            //    .HasRequired(pk => pk.Problem)
-            //    .WithMany(p => p.Keywords)
-            //    .HasForeignKey(pk => pk.ProblemId);
+            modelBuilder.Entity<ProblemAndKeyword>()
+                .HasRequired(pk => pk.Problem)
+                .WithMany(p => p.Keywords)
+                .HasForeignKey(pk => pk.ProblemId);
 
-            //modelBuilder.Entity<ProblemAndKeyword>()
-            //   .HasRequired(pk => pk.Keyword)
-            //   .WithMany(p => p.Problems)
-            //   .HasForeignKey(pk => pk.KeywordId);
+            modelBuilder.Entity<ProblemAndKeyword>()
+               .HasRequired(pk => pk.Keyword)
+               .WithMany(p => p.Problems)
+               .HasForeignKey(pk => pk.KeywordId);
 
-            //modelBuilder.Entity<Problem>()
-            //  .HasRequired(u => u.Author)
-            //    .WithMany(p => p.Problems)
-            //    .HasForeignKey(s => s.AuthorId);
+            modelBuilder.Entity<Problem>()
+              .HasRequired(u => u.Author)
+                .WithMany(p => p.Problems)
+                .HasForeignKey(s => s.AuthorId);
 
-            //modelBuilder.Entity<Article>()
-            //  .HasRequired(a => a.Author)
-            //    .WithMany(u => u.Articles)
-            //    .HasForeignKey(s => s.AuthorId);
+            modelBuilder.Entity<Article>()
+              .HasRequired(a => a.Author)
+                .WithMany(u => u.Articles)
+                .HasForeignKey(s => s.AuthorId);
 
             base.OnModelCreating(modelBuilder);
         }
