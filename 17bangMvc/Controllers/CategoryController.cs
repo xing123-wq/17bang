256befa33934fc83d04ad259f6f6c0776bc9ce65
龙泰 幸 <1978174217@qq.com>
2020-08-14 15:49:04 +0800

    系列存入数据，SeriesLevel有值，没有重复数据

--- 17bangMvc/Controllers/CategoryController.cs
+++ 17bangMvc/Controllers/CategoryController.cs
@@ -18,27 +18,23 @@ namespace _17bangMvc.Controllers
         [HttpGet]
         public ActionResult Manage()
         {
-            SeriesModel models = new SeriesModel();
-            models.SeriesModels = service.Get(service.CurrentUserId.Value);
-            return View(models);
-        }
-        [HttpGet]
-        public ActionResult _NewInManage()
-        {
             SeriesModel model = new SeriesModel();
             ViewData["SelectLists"] = service.GetSelectListItems(service.Get(service.CurrentUserId.Value));
+            model.SeriesModels = service.Get(service.CurrentUserId.Value);
             return View(model);
         }
+
         [HttpPost]
-        public ActionResult _NewInManage(SeriesModel model)
+        public ActionResult Manage(SeriesModel model)
         {
             if (!ModelState.IsValid)
             {
                 ViewData["SelectLists"] = service.GetSelectListItems(service.Get(service.CurrentUserId.Value));
-                return View("Manage", model);
+                model.SeriesModels = service.Get(service.CurrentUserId.Value);
+                return View(model);
             }
             service.Save(model);
-            return Redirect("/Category/Manage");
+            return Redirect("/Category/Manage ");
         }
 
     }
