0cfe0b165ba470d2ff062044416195b4942acfa6
龙泰 幸 <1978174217@qq.com>
2021-01-12 21:59:48 +0800

    /Article/index，JavaScript添加，当前用户是文章作者，显示修改按钮

--- 17bangMvc/Scripts/Global/JavaScript.js
+++ 17bangMvc/Scripts/Global/JavaScript.js
@@ -22,3 +22,18 @@ function showModal(title, body, nofooter) {
     }
     $modal.modal("show");
 }
+
+function getCookie(cookieName) {
+    var cookieValue = "";
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i];
+            if (cookie.substring(0, cookieName.length + 2).trim() == cookieName.trim() + "=") {
+                cookieValue = cookie.substring(cookieName.length + 2, cookie.length);
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
